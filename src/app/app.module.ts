6472207b769a1ed4c49754c0b60330a34c95d3cc
Amro Srouji <amro.srouji@gmail.com>
2020-01-16 11:37:46 +0100

    Add Login, Services Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ServicesComponent } from './pages/services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule
