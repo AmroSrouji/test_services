8abe3b2611e8833e0ace96e14eea59b0b7e21a1c
Amro Srouji <amro.srouji@gmail.com>
2020-01-16 12:47:13 +0100

    Add angular material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule} from '@angular/router';
 
+// Modules
+import {AngularMaterialModule} from "./angular.material.module";
+
 import {appRoutes} from "./app.routing";
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ServicesComponent } from './pages/services/services.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,9 @@ import { ServicesComponent } from './pages/services/services.component';
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,6 +3,7 @@ import {LoginComponent} from "./pages/login/login.component";
 import {ServicesComponent} from "./pages/services/services.component";
 
 export const appRoutes: Routes = [
+    { path: '', redirectTo: '/login',  pathMatch: 'full'},
     { path: 'login', component: LoginComponent },
     { path: 'services', component: ServicesComponent },
   ]
