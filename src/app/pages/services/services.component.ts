b9cb26bff34f337ec9c566df7e1a6fd0b5ccfe5d
Amro Srouji <amro.srouji@gmail.com>
2020-01-16 13:29:53 +0100

    Login Component Implementation, Service Component Implementation

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -8,9 +8,39 @@ import { config } from "../../app.config";
 })
 export class ServicesComponent implements OnInit {
 
+  services = [
+    {
+      serviceId:"1",
+      name:"Årsbokslut",
+      description: "description test",
+      active: false
+    },
+    {
+      serviceId:"2",
+      name:"Apaartment Cleaning Service",
+      description: "description test",
+      active: false
+    },
+    {
+      serviceId:"3",
+      name:"Laundry Bokings",
+      description: "description test",
+      active: false
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  onToggleService(serviceIndex){
+    this.services[serviceIndex].active = !this.services[serviceIndex].active;
+  }
+
+  onClickSubmit(){
+    const servicesIds = this.services.filter(service => service.active).map(activeService => activeService.serviceId);
+    console.log("Selected Services", servicesIds);
+  }
 }
